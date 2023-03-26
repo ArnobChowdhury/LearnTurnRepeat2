2421e7cb953cdd6c5603881373adc80d1e62467f
Arnob Chowdhury <chowdhury.sami.al@gmail.com>
2023-03-26 08:04:36 +0600

    Route loader added

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,41 @@
 import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
+import { useLoaderData } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import "./App.css";
 
 function App() {
-  const [state, setState] = useState("");
-  console.log(state);
+  const [topic, setTopic] = useState("");
+  console.log(topic);
+  const courses = useLoaderData() as { name: string }[];
+  console.log(courses);
+  let courseComponents;
+  if (Array.isArray(courses)) {
+    courseComponents = courses.map((course) => {
+      return (
+        <Card onClick={() => {}} sx={{ marginBottom: "20px" }}>
+          <CardContent>
+            <Typography sx={{ fontSize: "24px", padding: "6px" }}>
+              {course.name}
+            </Typography>
+          </CardContent>
+        </Card>
+      );
+    });
+  }
+
   return (
     <div>
+      {courseComponents}
       <label htmlFor="topic">Topics learned: </label>
       <input
         id="topic"
         name="topic"
         onChange={(e) => {
-          setState(e.currentTarget.value);
+          setTopic(e.currentTarget.value);
         }}
-        value={state}
+        value={topic}
         style={{ padding: "10px", width: "200px" }}
       ></input>
     </div>
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    loader: () => {
+      return [{ name: "Solidity" }, { name: "Flutter" }];
+    },
+  },
+  {
+    path: "courses/:course",
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
